b1fb09e4ef1cd09d7a9394b80fa1f9196bd600db
Dato Simó <dato@net.com.org.es>
2017-05-12 18:13:50 -0300

    Include bits from LAB3 that were not part of TP2.

--- lib/libmain.c
+++ lib/libmain.c
@@ -13,7 +13,9 @@ libmain(int argc, char **argv)
 {
 	// set thisenv to point at our Env structure in envs[].
 	// LAB 3: Your code here.
-	thisenv = 0;
+	envid_t id = sys_getenvid();
+	if (id >= 0)
+		thisenv = &envs[ENVX(id)];
 
 	// save the name of the program so that panic() can use it
 	if (argc > 0)
