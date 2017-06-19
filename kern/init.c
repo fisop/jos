02a45b0cd89cd7755027bc8a8d522c28f369a89c
Dato Simó <dato@net.com.org.es>
2017-06-19 01:00:21 -0300

    Create user_hello in TP3, and run it with env_run().

--- kern/init.c
+++ kern/init.c
@@ -59,8 +59,14 @@ i386_init(void)
 	ENV_CREATE(TEST, ENV_TYPE_USER);
 #else
 	// Touch all you want.
-	ENV_CREATE(user_primes, ENV_TYPE_USER);
+	ENV_CREATE(user_hello, ENV_TYPE_USER);
+	ENV_CREATE(user_hello, ENV_TYPE_USER);
+	ENV_CREATE(user_hello, ENV_TYPE_USER);
 #endif // TEST*
+
+	// Eliminar esta llamada una vez completada la parte 1
+	// e implementado sched_yield().
+	env_run(&envs[0]);
 
 	// Schedule and run the first user environment!
 	sched_yield();
